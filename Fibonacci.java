bac1c18e9f5bee0b69ba99c44e62f36bd1980cdb
Nikhill Sharma <132196283+ShanikGIT@users.noreply.github.com>
2023-09-06 12:48:35 +0530

    Add files via upload
    Fibonacci series without recursion

---
+++ Fibonacci.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class Fibonacci {
+    public static void main(String []args){
+     
+        Scanner me = new Scanner(System.in);
+        int n1 = 0,n2=1,n3;
+        int count = me.nextInt();
+        System.out.print(n1+" "+n2);
+        
+        for(int i = 0; i<count-2; i++){
+            n3=n1+n2;
+            System.out.print(" "+n3);
+            n1=n2;
+            n2=n3;
+          
+        }
+
+        
+        
+       
+
+    
+        
+        
+
+
+
+
+
+
+
+
+
+    }
+}
